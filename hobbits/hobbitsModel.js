d141b7824ca5d422f4017675a1766c4fadd1d0bb
umbur <you@example.com>
2019-03-14 15:09:16 -0600

    created db(using knex migration) and server using hobbits model from guided demo

---
+++ hobbits/hobbitsModel.js
@@ -0,0 +1,32 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById,
+};
+
+async function insert(hobbit) {
+  const [id] = await db('hobbits').insert(hobbit)
+  return db('hobbits')
+  .where({ id })
+  .first();
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+function getAll() {
+  return db('hobbits');
+}
+
+function findById(id) {
+  return null;
+}
